b453f8231b0fa768b46ca14f5e627677ad4afe51
Morten <morten.dahl@gmx.net>
2019-06-07 14:32:10 +0100

    scaling

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import codecs
 import keras
 import re
 from sklearn.preprocessing import OneHotEncoder
+from preprocessing import Min_Max_Train
 
 ################################### New methods #############################################
 def get_titanic_dataset():
@@ -160,6 +161,8 @@ def get_timeseries_dataset():
     X = df.drop(["RainTomorrow"], axis=1)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    X_train, X_test = Min_Max_Train(X_train, X_test)
     
     return X_train, X_test, y_train, y_test
 
