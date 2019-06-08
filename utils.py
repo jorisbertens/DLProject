9f8ed98467e1a96d6be8e6f1c36960d15bad39f6
Antonio Curado <1140280@isep.ipp.pt>
2019-06-08 16:11:43 +0100

    Added f1-measure

--- utils.py
+++ utils.py
@@ -225,7 +225,6 @@ def get_text_dataset():
     tokenizer.fit_on_texts(df['text'].values)
     X = tokenizer.texts_to_sequences(df['text'].values)
     X = pad_sequences(X)
-
     X_train, X_test, y_train, y_test = train_test_split(pd.DataFrame(X), df['y'], test_size=0.3, shuffle=True)
 
     return X_train, X_test, y_train, y_test
@@ -423,6 +422,39 @@ def split_sequences(sequences, n_steps):
         y.append(seq_y)
     return array(X), array(y)
 
+
+
+from keras import backend as K
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 ################################### Old methods #############################################
 def get_dataset():
